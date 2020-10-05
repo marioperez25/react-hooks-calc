88929df492b77b495e01cce308b32441c167983d
marioperez25 <mariofph@gmail.com>
2020-10-04 20:58:06 -0600

    calculator with hooks

---
+++ src/calculator/Calculator.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+
+export default function Calculator(){
+    
+    const [result = Int8Array, setResult] = useState(0);
+
+    const [inputField = Int8Array, setInputField] = useState(0);
+
+    const handleChange = event => {
+        debugger;
+        if (parseInt(event.target.value) !== isNaN) {
+           return setInputField(parseInt(event.target.value));
+        }
+        return setInputField(inputField);
+    }
+
+    const add = event =>{
+        setResult(result + inputField);
+        setInputField(0);
+    }
+
+    const minus = event =>{
+        setResult(result - inputField);
+        setInputField(0);
+    }
+
+    const dividedBy = event =>{
+        setResult(result / inputField);
+        setInputField(0);
+    }
+
+    const multipliedBy = event =>{
+        setResult(result * inputField);
+        setInputField(0);
+    }
+    
+    return (
+        <div>
+            <p>Result= {result}</p>
+            <input type="text" value={inputField} onChange={handleChange}/>
+            <button onClick={() => setResult(add)}>
+             +
+            </button>
+            <button onClick={() => setResult(minus)}>
+             -
+            </button>
+            <button onClick={() => setResult(dividedBy)}>
+            /
+            </button>
+            <button onClick={() => setResult(multipliedBy)}>
+            *
+            </button>
+            <button onClick={() => setResult(0)}>
+            reset
+            </button>
+        </div>
+    );
+}
