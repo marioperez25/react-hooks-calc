50cb4e33144088eccb9c85b78491663f02090c31
marioperez25 <mariofph@gmail.com>
2020-10-04 22:45:31 -0600

    fixed divisions by 0

--- src/calculator/Calculator.js
+++ src/calculator/Calculator.js
@@ -1,10 +1,30 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 
 export default function Calculator(){
     
     const [result = Int8Array, setResult] = useState(0);
 
     const [inputField = Int8Array, setInputField] = useState(0);
+
+    const Button = styled.button`
+        /* Adapt the colors based on primary prop */
+        background: ${props => props.primary ? "palevioletred" : "white"};
+        color: ${props => props.primary ? "white" : "palevioletred"};
+        text-align: center;
+        font-size: 1em;
+        margin: 1em;
+        padding: 0.25em 1em;
+        border: 2px solid palevioletred;
+        border-radius: 3px;
+        `;
+    
+    const Buttons = styled.div`
+        display: grid;
+        grid-template-columns: auto auto auto;
+        max-width: 600px;
+        margin: auto;
+    `;
 
     const handleChange = event => {
         if (parseInt(event.target.value) !== isNaN) {
@@ -24,8 +44,15 @@ export default function Calculator(){
     }
 
     const dividedBy = event =>{
-        setResult(result / inputField);
-        setInputField(0);
+        if (inputField === 0) {
+            setResult(result);
+            setInputField(0);
+        } else {
+            setResult(result / inputField);
+            setInputField(0);
+        }
+
+        
     }
 
     const multipliedBy = event =>{
@@ -37,21 +64,23 @@ export default function Calculator(){
         <div>
             <p>Result= {result}</p>
             <input type="number" value={inputField} onChange={handleChange}/>
-            <button onClick={() => setResult(add)}>
-             +
-            </button>
-            <button onClick={() => setResult(minus)}>
-             -
-            </button>
-            <button onClick={() => setResult(dividedBy)}>
-            /
-            </button>
-            <button onClick={() => setResult(multipliedBy)}>
-            *
-            </button>
-            <button onClick={() => setResult(0)}>
-            reset
-            </button>
+            <Buttons>
+                <Button primary onClick={() => setResult(add)}>
+                +
+                </Button>
+                <Button onClick={() => setResult(minus)}>
+                -
+                </Button>
+                <Button onClick={() => setResult(dividedBy)}>
+                /
+                </Button>
+                <Button onClick={() => setResult(multipliedBy)}>
+                *
+                </Button>
+                <Button onClick={() => setResult(0)}>
+                reset
+                </Button>
+            </Buttons>
         </div>
     );
 }
